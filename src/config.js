1f4112b550c5ac3eb5c391e8d7323c2e617a23e7
chickentooth <mr.kurro@gmail.com>
2018-10-27 10:06:35 +0700

    sd

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // export const API_BASE = 'https://damp-scrubland-52881.herokuapp.com/api/v1';
-export const API_BASE = 'http://localhost:3000/api/v1';
-export const BACKEND_HOST = 'http://localhost:3000';
+export const API_BASE = 'http://35.207.198.238:3000/api/v1';
+export const BACKEND_HOST = 'http://35.207.198.238:3000';
 /*
 export const API_BASE = 'http://localhost:3000/api/v1';
 export const BACKEND_HOST = 'http://localhost:3000';*/
